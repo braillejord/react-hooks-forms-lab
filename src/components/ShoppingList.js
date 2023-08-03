5abb8714622f3d6c7e3575c9fe0236886e78d731
BreElle Wells <breellejordyn@gmail.com>
2023-08-03 16:02:03 -0600

    complete filter deliverable

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,32 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearchChange] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function onSearchChange(e) {
+    setSearchChange(() => e.target.value)
 
-    return item.category === selectedCategory;
-  });
+  }
+  console.log(search)
+
+  const itemsToDisplay = items
+    .filter((item) => {
+      if (selectedCategory === "All") return true;
+
+      return item.category === selectedCategory;
+    })
+    .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+  // .filter(item => item.name.includes(search))
+
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} search={search} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
